1e8ce6f8a46db059eb489d979195e21495e64e24
Theradox <bojan.spasovski26@gmail.com>
2021-02-10 22:52:26 +0100

    Initial commit

---
+++ src/main/java/com/app/autcards/AutcardsApplication.java
@@ -0,0 +1,13 @@
+package com.app.autcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutcardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutcardsApplication.class, args);
+    }
+
+}
