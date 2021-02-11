67a6aa7e15056eac8e2d0fbaa2565559a808fba4
Andrej <savatikjandrej@gmail.com>
2021-02-11 13:09:05 +0100

    Start

---
+++ src/main/java/com/app/autcards/model/Card.java
@@ -0,0 +1,5 @@
+package com.app.autcards.model;
+
+public class Card {
+    Long id;
+}
---
+++ src/main/java/com/app/autcards/model/Deck.java
@@ -0,0 +1,15 @@
+package com.app.autcards.model;
+
+import java.util.List;
+
+public class Deck {
+    private Long id;
+
+    private String name;
+
+    private List<Card> cards;
+
+    public Deck(){}
+
+
+}
