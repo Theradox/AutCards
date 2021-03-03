54283a79cc6aaaba7627fa864583a3cd20d3c2bf
Theradox <bojan.spasovski26@gmail.com>
2021-03-03 09:21:13 +0100

    fix max char limit on answer, added textarea instead of input

--- src/main/java/com/app/autcards/model/Card.java
+++ src/main/java/com/app/autcards/model/Card.java
@@ -4,11 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Entity
@@ -25,6 +23,8 @@ public class Card {
     private String question;
 
     @NotNull
+    @Column(length = 1000)
+    @Size(min = 3, max = 1000)
     private String answer;
 
     private LocalDateTime postponeDate;
